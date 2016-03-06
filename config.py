5bc3ebdbd4c59017be16618f22d332e44ab81913
Diego Rueda <sandiego206@gmail.com>
2016-03-05 22:47:08 -0500

    add config file

---
+++ config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+# Database settings
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
