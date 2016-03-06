ccf2f3d17d50c81f1e73a7e7ee0c5520e6fe1dce
Diego Rueda <sandiego206@gmail.com>
2016-03-06 01:39:25 -0500

    create simple cart, delete items from cart

--- app/views.py
+++ app/views.py
@@ -3,32 +3,45 @@ from app import app, db
 from models import Product, Cart, LineItem
 
 
+def get_all_products():
+    return Product.query.all()
+
+
+def get_current_cart():
+    try:
+        return Cart.query.get(session['cart'])
+    except:
+        return None
+
+
+def cart_is_empty(cart):
+    return 0 if db.session.query(LineItem).filter_by(cart_id=cart.id).first() else 1
+
+
 @app.route('/')
 @app.route('/index')
 def index():
-    products = Product.query.all()
     return render_template('index.html',
                            title='Index',
-                           products=products)
+                           products=get_all_products(),
+                           cart=get_current_cart())
 
 
 @app.route('/add/<int:product_id>')
 def add(product_id):
     product = Product.query.get(product_id)
     # Checks to see if the user has already started a cart.
-    cart = Cart.query.get(session['cart'])
-    if cart is not None:
-        # If line item does not exist, create it
-        l_item = LineItem.query.get((cart.id, product.id))
-        if l_item is None:
-            l_item = LineItem(cart_id=cart.id, product_id=product.id, quantity=1)
-        # If line item exists, just update quantity
-        else:
-            l_item.quantity += 1
-    else:
+    cart = get_current_cart()
+    if cart is None:
+        # Create cart and add line_item
         cart = Cart()
-        session['cart'] = cart.id
+
+    # Check for a line_item, if it doesn't exists create it, if it does update it.
+    l_item = LineItem.query.get((cart.id, product.id))
+    if l_item is None:
         l_item = LineItem(cart_id=cart.id, product_id=product.id, quantity=1)
+    else:
+        l_item.quantity += 1
 
     product.line_item.append(l_item)
     cart.line_item.append(l_item)
@@ -36,4 +49,22 @@ def add(product_id):
     db.session.add(product)
     db.session.add(l_item)
     db.session.commit()
-    return str(LineItem.query.get((1, product.id)).quantity)
+    session['cart'] = cart.id
+    return redirect('index')
+
+
+@app.route('/remove/<int:product_id>')
+def remove(product_id):
+    # Remove product and commit changes
+    product = Product.query.get(product_id)
+    cart = get_current_cart()
+    l_item = LineItem.query.get((cart.id, product.id))
+    db.session.delete(l_item)
+    db.session.commit()
+
+    # if the cart is empty, delete it
+    if cart_is_empty(cart):
+        session.pop('cart')
+        db.session.delete(cart)
+        db.session.commit()
+    return redirect('index')
